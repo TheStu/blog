381cf5e61dbc7ec07c80432a2103ca833cea56a4
Stuart Morris <stuwashere@gmail.com>
2013-08-02 16:39:40 -0700

    fix image pathway helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,11 +34,11 @@ module ApplicationHelper
 
   def fix_image_pathway(text) #adds the random hash into the image pathway for heroku
     if Rails.env.production?
-      text.scan(/src="([^.]+.jpg)"/).each do |src|
+      text.scan(/src="(\/assets\/[^.]+.jpg)"/).each do |src|
         src.first.match(/\/([^.\/]+.jpg)/)
-        text = text.gsub(src.first, path_to_image($1))
+        text = text.gsub(src.first, image_path($1))
       end
-      return text
     end
+    return text
   end
 end
