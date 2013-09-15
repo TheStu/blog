2e5dc909c630312c0269393570e3a8acc2555bce
Stuart Morris <stuwashere@gmail.com>
2013-09-14 19:27:47 -0700

    remove what_i_say from required params for retailers

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -2,5 +2,5 @@ class Retailer < ActiveRecord::Base
   has_many :internal_categorizations, as: :internal_categorizable, dependent: :destroy
   has_many :internal_categories, :through => :internal_categorizations
 
-  validates_presence_of :name, :url, :what_i_say
+  validates_presence_of :name, :url
 end
